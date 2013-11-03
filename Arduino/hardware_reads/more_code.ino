4727f3c4bfe346db155a4f9a02e467889e2a3251
Dave Hand <davehand13@gmail.com>
2013-11-03 18:34:14 -0500

    Added GPS, wifi, and hardware basic function code

---
+++ Arduino/hardware_reads/more_code.ino
@@ -0,0 +1,23 @@
+int relay=53;
+int cross=24;
+bool state;
+bool prestate=0;
+
+void setup() {
+  pinMode(relay,OUTPUT);
+  pinMode(cross,INPUT);
+ // Serial.begin(9600);
+
+
+}
+
+void loop() {
+state=digitalRead(cross);
+if(state!=prestate){
+ // Serial.println(millis());
+}
+digitalWrite(relay,!state);
+
+prestate=state;
+  
+}
---
+++ Arduino/wifi/TinyGPS.ino
@@ -0,0 +1,50 @@
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>                // Special version for 1.0
+
+TinyGPS gps;
+SoftwareSerial nss(6, 255);            // Yellow wire to pin 6
+
+void setup() {
+  Serial.begin(115200);
+  nss.begin(4800);
+  Serial.println("Reading GPS");
+}
+
+void loop() {
+  bool newdata = false;
+  unsigned long start = millis();
+  while (millis() - start < 8) {  // Update every 5 seconds
+    if (feedgps()){
+           newdata = true;
+    }
+  }
+  if (newdata) {
+    gpsdump(gps);
+  }
+}
+
+// Get and process GPS data
+void gpsdump(TinyGPS &gps) {
+  float flat, flon;
+  unsigned long age, date, time, fix_age;
+  gps.f_get_position(&flat, &flon, &age);
+  gps.get_datetime(&date, &time, &fix_age);
+  
+  int year;
+  byte month, day, hour, minute, second, hundredths;
+
+  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &fix_age);
+  Serial.print(flat, 4); Serial.print(", "); 
+  Serial.println(flon, 4);
+  Serial.print(hour); Serial.print(":"); Serial.print(minute);Serial.print(":");Serial.println(second);
+ 
+}
+
+// Feed data as it becomes available 
+bool feedgps() {
+  while (nss.available()) {
+    if (gps.encode(nss.read()))
+      return true;
+  }
+  return false;
+}
